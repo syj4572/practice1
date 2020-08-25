56c18b064823b78ab6ac732b55517f362bd15c22
Shin Yeon Jung <syj0510@naver.com>
2020-08-25 13:55:10 +0900

    Added view and template for detailed blog post

--- practice/views.py
+++ practice/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import render
 from .models import Post
@@ -6,3 +7,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'post_list.html', {'posts':posts})
 
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post':post})
