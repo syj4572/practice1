5c3f71efa51f37509e3eb72f2a8c8f5f179721fc
Shin Yeon Jung <syj0510@naver.com>
2020-08-25 12:48:09 +0900

    Modified templates to display posts from database

--- practice/views.py
+++ practice/views.py
@@ -1,4 +1,8 @@
+from django.utils import timezone
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'post_list.html', {'posts':posts})
+
